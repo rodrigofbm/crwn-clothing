0d1090c5813d9404671c4f46e97d3c4074c1f487
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2019-12-18 00:09:58 -0300

    added remove and increase item to checkout-item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,23 @@ export const addItemToCart = (cartItems, cartItemToadd) => {
 
   return [...cartItems, { ...cartItemToadd, quantity: 1 }];
 };
+
+export const clearItemFromCart = (cartItem, itemToClear) => {
+  return cartItem.filter(cartItem => cartItem.id !== itemToClear.id);
+};
+
+export const removeItem = (cartItem, itemToRemove) => {
+  const existingCartItem = cartItem.find(
+    cartItem => cartItem.id === itemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return clearItemFromCart(cartItem, itemToRemove);
+  }
+
+  return cartItem.map(item =>
+    item.id === itemToRemove.id
+      ? { ...item, quantity: item.quantity - 1 }
+      : item
+  );
+};
